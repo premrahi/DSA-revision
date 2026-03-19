906fc227e7d33ab74678feed06f29ce914850620
premrahi <premrahi234@gmail.com>
2026-03-19 19:26:02 +0530

    binary search

---
+++ javascript/BinarySearch.ts
@@ -0,0 +1,15 @@
+function search(nums: number[], target: number): number {
+    let start: number = 0;
+    let end: number = nums.length - 1;
+
+    while (start <= end) {
+        let mid: number = Math.floor((start + end) / 2);
+
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] > target) end = mid - 1;
+        else {
+            start = mid + 1;
+        }
+    }
+    return -1;
+};
