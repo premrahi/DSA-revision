e9fc1f9db1505e56dc7a2df34a108d88ce3abc52
premrahi <premrahi234@gmail.com>
2026-07-03 14:58:44 +0530

    two pointer/square_of_sorted_array.cpp

---
+++ two pointer/square_of_sorted_array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        vector<int> res(nums.size()) ;
+        int left_ptr = 0 , right_ptr = nums.size() - 1;
+
+        int move_ptr = nums.size() - 1;
+
+        while(left_ptr <= right_ptr){
+            if(abs(nums[left_ptr]) > abs(nums[right_ptr])){
+                res[move_ptr--] =nums[left_ptr] *nums[left_ptr] ;
+                left_ptr++; 
+            }else{
+                res[move_ptr--] =nums[right_ptr] *nums[right_ptr] ; 
+                right_ptr--;
+            }
+        }
+        return res ;       
+        
+    }
+};
