8e10943978888feadf09b33c7f9ca494e6bf7be5
premrahi <premrahi234@gmail.com>
2026-07-09 13:42:19 +0530

    middle of LL

---
+++ Fast and slow pointer/MiddleOfLL.js
@@ -0,0 +1,11 @@
+var middleNode = function(head) {
+    let fast = head ;
+    let slow = head ;
+
+    while(fast !== null && fast.next !== null){
+        slow = slow.next ;
+        fast = fast.next.next ; 
+    }
+
+    return slow ;
+};
